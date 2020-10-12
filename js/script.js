4cef80d3ef80417a94cd54cfa8bb45b913c0e626
Athul  P <athul@regalix.com>
2020-10-12 14:11:44 +0530

    updated mock

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function minimize() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("minimize");
+   var elements = document.getElementById("myDIVs");
+   elements.classList.toggle("maximize");
+}
